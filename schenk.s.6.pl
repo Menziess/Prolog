95d4aef7d1e0147e6c42f3b5c7a8b6d1ee23d44d
stefan <stefan_schenk@hotmail.com>
2017-10-19 16:55:15 +0200

    Updated lat-longs.

--- schenk.s.6.pl
+++ schenk.s.6.pl
@@ -35,18 +35,18 @@ route(VertrekStad, AankomstStad, Route, Distance) :-
 */
 
 %     Stadnaam    N.B.    O.L.
-city("amsterdam"  /52.23  /4.54).
-city("breda"      /51.59  /4.82).
-city("enkhuizen"  /52.42  /5.17).
-city("enschede"   /52.13  /6.54).
-city("den helder" /52.56  /4.46).
-city("groningen"  /53.13  /6.34).
-city("haarlem"    /52.37  /4.66).
-city("hoorn"      /52.39  /5.04).
-city("rotterdam"  /51.55  /4.29).
-city("maastricht" /50.51  /5.41).
-city("roosendaal" /51.32  /4.28).
-city("utrecht"    /52.06  /5.07).
+city("amsterdam"  /52.37  /4.89).
+city("breda"      /51.57  /4.77).
+city("enkhuizen"  /52.71  /5.27).
+city("enschede"   /52.22  /6.89).
+city("den helder" /52.96  /4.76).
+city("groningen"  /53.22  /6.57).
+city("haarlem"    /52.39  /4.65).
+city("hoorn"      /52.64  /5.06).
+city("rotterdam"  /51.92  /4.48).
+city("maastricht" /50.85  /5.69).
+city("roosendaal" /51.54  /4.47).
+city("utrecht"    /52.09  /5.12).
 
 /*
 |-------------------------------------------------------------------------------
@@ -56,6 +56,19 @@ city("utrecht"    /52.06  /5.07).
 
 estimate(X, Y) :-
   write("").
+
+% Berekent hemelsbreede afstand tussen twee steden, bijvoorbeeld: de afstand
+% tussen Amsterdam en Enhuizen wordt geschat op 47,7 km.
+distance_between(City1, City2, Dis) :-
+  city(City1/Lat1/Lon1),
+  city(City2/Lat2/Lon2),
+  distance(Lat1, Lon1, Lat2, Lon2, Dis).
+
+% Method to determine distance between two lat/lon coordinates.
+distance(Lat1, Lon1, Lat2, Lon2, Dis) :-
+    P is 0.017453292519943295,
+    A is (0.5 - cos((Lat2 - Lat1) * P) / 2 + cos(Lat1 * P) * cos(Lat2 * P) * (1 - cos((Lon2 - Lon1) * P)) / 2),
+    Dis is (12742 * asin(sqrt(A))).
 
 /*
 |-------------------------------------------------------------------------------
